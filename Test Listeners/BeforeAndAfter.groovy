7fdc1d4cb68bc523a1245ebf8c6f84ef7ec4e815
George Margaritis <george.m@weblicity.com>
2018-07-16 18:42:32 +0300

    Create test listeners for before and after actions for each test
    Signed-off-by: George Margaritis <george.m@weblicity.com>

---
+++ Test Listeners/BeforeAndAfter.groovy
@@ -0,0 +1,26 @@
+import com.kms.katalon.core.annotation.AfterTestCase
+import com.kms.katalon.core.annotation.BeforeTestCase
+import com.kms.katalon.core.context.TestCaseContext
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+class BeforeAndAfter {
+	/**
+	 * Executes before every test case starts.
+	 * @param testCaseContext related information of the executed test case.
+	 */
+	@BeforeTestCase
+	def sampleBeforeTestCase(TestCaseContext testCaseContext) {
+		WebUI.openBrowser('https://www.wg-gesucht.de/en')
+		WebUI.maximizeWindow()
+		//WebUI.navigateToUrl('https://www.wg-gesucht.de/en/angebot-aufgeben.html')
+	}
+
+	/**
+	 * Executes after every test case ends.
+	 * @param testCaseContext related information of the executed test case.
+	 */
+	@AfterTestCase
+	def sampleAfterTestCase(TestCaseContext testCaseContext) {
+		//WebUI.closeBrowser()
+	}
+}
